1838c52574ab233fbd3064d347be0ba7ae5e5f45
adam <adam@adamsmith.as>
2009-08-10 01:23:44 -0700

    candidacy

--- pitch_tent.py
+++ pitch_tent.py
@@ -1,4 +1,4 @@
 from twisted.web import server, static
 from twisted.internet import reactor
-reactor.listenTCP(8080, server.Site(static.File(".")))
+reactor.listenTCP(8088, server.Site(static.File(".")))
 reactor.run()
